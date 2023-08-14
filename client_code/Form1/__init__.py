4f12ba7663c547bb39188c64e0890174b7125bbc
oliverduce@googlemail.com <oliverduce@googlemail.com>
2023-08-14 08:31:33 +0000

    Created Form1.ItemTemplate1; Edited settings, ServerModule1 and Form1

--- client_code/Form1/__init__.py
+++ client_code/Form1/__init__.py
@@ -9,5 +9,15 @@ class Form1(Form1Template):
   def __init__(self, **properties):
     # Set Form properties and Data Bindings.
     self.init_components(**properties)
+    self.repeating_panel_tasks.items = anvil.server.call('get_tasks')
+    
 
     # Any code you write here will run before the form opens.
+
+  def button_add_click(self, **event_args):
+    """This method is called when the button is clicked"""
+    task = self.text_box_new_task.text
+    anvil.server.call('add_to_do_item_to_data_table',task)
+    self.text_box_new_task.text = ""
+    
+
--- server_code/ServerModule1.py
+++ server_code/ServerModule1.py
@@ -3,5 +3,10 @@ import anvil.tables.query as q
 from anvil.tables import app_tables
 import anvil.server
 
-def add_to_do_item_to_data_table(item):
-  app_tables.to_do_items.
+@anvil.server.callable
+def add_to_do_item_to_data_table(task):
+  app_tables.tasks.add_row(task = task, done = False)
+
+@anvil.server.callable
+def get_tasks():
+  return app_tables.tasks.search()
